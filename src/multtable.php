411a4fb5a2d1c0ba6eb015d8a5016c2991a01b38
Shaun Stapleton <staplets@onid.oregonstate.edu>
2014-11-02 18:28:47 -0500

    Add Assignment 5 folders. All files complete.

--- src/multtable.php
+++ src/multtable.php
@@ -1,16 +1,16 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors','On');
-
+  //check for empty parameters
   if(empty($_GET)){
     echo "Nothing passed in URL.";
 	exit();
-  }
+  }//check if parameters are numeric
   foreach($_GET as $key=>$value){
     if(!is_numeric($value)){
 	  echo $key . " must be an integer.<br>";
 	}
-  }
+  }//get min and max parameters
   if(isset($_GET['min-multiplicand'])){
     $min_multiplicand = intval($_GET['min-multiplicand']);
   }
@@ -39,7 +39,7 @@ ini_set('display_errors','On');
     echo "Missing parameter max-multiplier. <br>";
 	exit();
   }
-
+  //check if max is larger than min
   if($min_multiplicand > $max_multiplicand){
     echo "Minimum multiplicand larger than maximum multiplicand. <br>";
     exit();
@@ -48,29 +48,30 @@ ini_set('display_errors','On');
     echo "Minimum multiplier larger than maximum multiplier. <br>";
     exit();
   }
+  //assign variables for multidimensional array
   $minCanHolder = $min_multiplicand;
   $minPliHolder = $min_multiplier;
   $width = $max_multiplicand - $min_multiplicand + 2;
   $height = $max_multiplier - $min_multiplier + 2;
-  
+  //create array for multiplicands
   $candArray = array();
   while($min_multiplicand <= $max_multiplicand){
     $candArray[] = $min_multiplicand;
 	$min_multiplicand++;
-  }
+  }// creat array for multipliers
   $plierArray = array();
   while($min_multiplier <= $max_multiplier){
     $plierArray[] = $min_multiplier;
 	$min_multiplier++;
   }
-
+  //set up table
   echo '<html>
   <!DOCTYPE html>
   <html lang="en">
   <meta charset="utf=8" />
     <h2>My Multiplication table<h2>
     <body>';
-  
+  //create multidimensional array for output
   $tableArray = array();
   for($i = $minCanHolder; $i <= $max_multiplicand; $i++){
     $tableArray[] = array();
@@ -87,7 +88,7 @@ ini_set('display_errors','On');
   <table>
   <table border="1">
   <tr>';
-  
+  //output table
   foreach($tableArray as $key => $value){
     echo '<tr>';
     foreach($value as $layerTwo => $twoVal){
